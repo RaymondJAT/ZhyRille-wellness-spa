8e6e0b6bd3fc94af71c0292f398ea3795bd272bd
RaymondJAT <rjat081700@gmail.com>
2024-04-15 23:49:23 +0800

    tw-elements-react Added

--- ZhyRille/src/App.jsx
+++ ZhyRille/src/App.jsx
@@ -2,12 +2,14 @@ import React from "react";
 import "./App.css";
 import HomePage from "./components/HomePage/home/Homes";
 import About from "./components/AboutSection/About";
+import Modal from "./components/Modal/Modal";
 
 function App() {
   return (
     <>
       <HomePage />
       <About />
+      <Modal />
     </>
   );
 }

16adaf1bd3f0b33722f6d223d77368a7cc83b79f
RaymondJAT <rjat081700@gmail.com>
2024-04-20 19:11:14 +0800

    Carousel Added

--- ZhyRille/src/components/TestimonialSection/Testimonial.jsx
+++ ZhyRille/src/components/TestimonialSection/Testimonial.jsx
@@ -1,54 +1,59 @@
 import React from "react";
+import { TECarousel, TECarouselItem } from "tw-elements-react";
 
 const Testimonial = () => {
   return (
     <div className="container relative w-full min-h-72 ">
       <div className="contents-wrapper w-[70%] min-h-inherit my-7 mx-auto text-center">
         <section className="header">
-          <h1 className="relative text-4xl uppercase font-bold text-center tracking-wide before:contents before:w-[200px] before:h-[2px] before:bg-yellow-500 before:rounded-lg before:absolute before:left[50%] before:translate-x-1/2 before:-bottom-2.5">
+          <h1 className="relative text-4xl uppercase font-bold text-center tracking-wide before:content-[''] before:w-[150px] before:h-[2px] before:bg-yellow-500 before:rounded-lg before:absolute before:left[50%] before:translate-x-1/2 before:-bottom-2.5">
             Testimonial
           </h1>
         </section>
 
-        <section className="testRow w-full min-h-inherit relative overflow-hidden">
-          <div className="testItem w-full h-full absolute flex justify-center items-center flex-col">
-            <h3 className="text-xl italic p-2">Name here</h3>
-            <h4 className="italic">Lorem ipsum dolor</h4>
-            <p className="text-sm tracking-wide p-3">
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Veritatis
-              aperiam distinctio ea unde eligendi exercitationem nam animi ex
-              quis aliquam!
-            </p>
+        <TECarousel showControls ride="carousel">
+          <div className="relative w-3/4 overflow-hidden after:clear-both after:block after:content-[''] mx-auto">
+            <TECarouselItem
+              itemID={1}
+              className="relative float-left -mr-[100%] hidden w-full transition-transform duration-[600ms] ease-in-out motion-reduce:transition-none text-xs"
+            >
+              <h3>Name here</h3>
+              <h4>Lorem ipsum dolor</h4>
+              <p>
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do
+                eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+                enim ad minim veniam, quis nostrud exercitation ullamco laboris
+                nisi ut aliquip ex ea commodo consequat.
+              </p>
+            </TECarouselItem>
+            <TECarouselItem
+              itemID={2}
+              className="relative float-left hidden -mr-[100%] w-full transition-transform duration-[600ms] ease-in-out motion-reduce:transition-none text-xs"
+            >
+              <h3>Name here</h3>
+              <h4>Lorem ipsum dolor</h4>
+              <p>
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do
+                eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+                enim ad minim veniam, quis nostrud exercitation ullamco laboris
+                nisi ut aliquip ex ea commodo consequat.
+              </p>
+            </TECarouselItem>
+            <TECarouselItem
+              itemID={3}
+              className="relative float-left -mr-[100%] hidden w-full transition-transform duration-[600ms] ease-in-out motion-reduce:transition-none text-xs"
+            >
+              <h3>Name here</h3>
+              <h4>Lorem ipsum dolor</h4>
+              <p>
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do
+                eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+                enim ad minim veniam, quis nostrud exercitation ullamco laboris
+                nisi ut aliquip ex ea commodo consequat.
+              </p>
+            </TECarouselItem>
           </div>
-        </section>
-
-        <section className="indicators absolute bottom-8 left-[50%] translate-x-1/2 p-1 cursor-pointer">
-          <div
-            className="dot w-4 h-4 my-0 mx-1 border-3 border-solid border-gray-400 rounded-[50%] inline-block transition-colors duration-150 ease-in active bg-yellow-500"
-            attr="0"
-            onClick="switchText(this)"
-          ></div>
-          <div
-            className="dot w-4 h-4 my-0 mx-1 border-3 border-solid border-gray-400 rounded-[50%] inline-block transition-colors duration-150 ease-in active bg-yellow-500"
-            attr="1"
-            onClick="switchText(this)"
-          ></div>
-          <div
-            className="dot w-4 h-4 my-0 mx-1 border-3 border-solid border-gray-400 rounded-[50%] inline-block transition-colors duration-150 ease-in active bg-yellow-500"
-            attr="2"
-            onClick="switchText(this)"
-          ></div>
-          <div
-            className="dot w-4 h-4 my-0 mx-1 border-3 border-solid border-gray-400 rounded-[50%] inline-block transition-colors duration-150 ease-in active bg-yellow-500"
-            attr="3"
-            onClick="switchText(this)"
-          ></div>
-          <div
-            className="dot w-4 h-4 my-0 mx-1 border-3 border-solid border-gray-400 rounded-[50%] inline-block transition-colors duration-150 ease-in active bg-yellow-500"
-            attr="4"
-            onClick="switchText(this)"
-          ></div>
-        </section>
+        </TECarousel>
       </div>
 
       <script src=""></script>
